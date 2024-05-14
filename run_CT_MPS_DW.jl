03c8553af1bdb27c9f4bbffa56e7ead92efbefa8
hainingpan <hnpan@terpmail.umd.edu>
2024-05-14 17:47:14 -0500

    update run_CT_MPS_DW.jl

--- run_CT_MPS_DW.jl
+++ run_CT_MPS_DW.jl
@@ -12,27 +12,31 @@ using ArgParse
 using Serialization
 """ compute domain wall as a function of t"""
 
-function run_dw_t(L::Int,p::Float64,seed::Int)
+function run_dw_t(L::Int,p_ctrl::Float64,p_proj::Float64,seed::Int)
     ct=CT.CT_MPS(L=L,seed=seed,folded=true,store_op=false,store_vec=false,ancilla=0,xj=Set([0]),x0=1//2^(L÷2+1))
     i=L
     tf=(ct.ancilla ==0) ? 2*ct.L^2 : div(ct.L^2,2)
     dw_list=zeros(tf+1,2)
     dw_list[1,:]=collect(CT.dw(ct,1))
     for idx in 1:tf
-        i=CT.random_control!(ct,i,p,)
+        i=CT.random_control!(ct,i,p_ctrl,p_proj)
         dw_list[idx+1,:]=collect(CT.dw(ct,(i%ct.L)+1))
     end
-    return Dict("DW"=>dw_list)
+    return Dict("DW1"=>dw_list[:,1],"DW2"=>dw_list[:,2])
 end
 
 
 function parse_my_args()
     s = ArgParseSettings()
     @add_arg_table! s begin
-        "--p", "-p"
+        "--p_ctrl"
         arg_type = Float64
         default = 0.0
-        help = "measurement rate"
+        help = "control rate"
+        "--p_proj"
+        arg_type = Float64
+        default = 0.0
+        help = "projection rate"
         "--L", "-L"
         arg_type = Int
         default = 8
@@ -49,9 +53,9 @@ function main()
     println("Uses threads: ",BLAS.get_num_threads())
     println("Uses backends: ",BLAS.get_config())
     args = parse_my_args()
-    results = run_dw_t(args["L"], args["p"], args["seed"])
+    results = run_dw_t(args["L"], args["p_ctrl"], args["p_proj"], args["seed"])
 
-    filename = "MPS_(0,1)_L$(args["L"])_p$(@sprintf("%.3f", args["p"]))_s$(args["seed"])_DW.json"
+    filename = "MPS_(0,1)_L$(args["L"])_pctrl$(@sprintf("%.3f", args["p_ctrl"]))_pproj$(@sprintf("%.3f", args["p_proj"]))_s$(args["seed"])_DW.json"
     data_to_serialize = merge(results, Dict("args" => args))
     json_data = JSON.json(data_to_serialize)
     open(filename, "w") do f
@@ -66,4 +70,4 @@ end
 
 
 
-# julia --sysimage ~/.julia/sysimages/sys_itensors.so run_CT_MPS_DW.jl --p 1 --L 8 --seed 0 
+# julia --sysimage ~/.julia/sysimages/sys_itensors.so run_CT_MPS_DW.jl --p_ctrl 0.5 --p_proj 0.5 --L 8 --seed 0 
