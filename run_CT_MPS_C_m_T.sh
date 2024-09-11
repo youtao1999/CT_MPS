108c1b99b5e39a767007ba32944f181d5d20e181
hainingpan <hnpan@terpmail.umd.edu>
2024-09-11 14:29:06 -0500

    update FM_CT_MPS_C_m_T

---
+++ run_CT_MPS_C_m_T.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hello OSPool from Job running on `whoami`@`hostname`" 
+mkdir -p CT/src
+mv Project.toml CT
+mv Manifest.toml CT
+mv CT.jl CT/src
+
+export JULIA_DEPOT_PATH=/srv/.julia:/usr/local/julia/share/julia
+
+julia run_CT_MPS_C_m_T.jl --p_ctrl $1 --p_proj $2 --L $3 --seed_C $4 --seed_m $5
