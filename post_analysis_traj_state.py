d307c12dea2f4b8b6bc9e43de1a75a5a9eaf34b4
Haining Pan <hnpan@terpmail.umd.edu>
2025-01-06 16:38:45 -0500

    update post_analysis_traj_state.py to improve speed

--- post_analysis_traj_state.py
+++ post_analysis_traj_state.py
@@ -2,7 +2,6 @@
 import sys
 dir_path='../control_transition'
 sys.path.append(dir_path)
-# import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from plot_utils import *
@@ -43,21 +42,22 @@ for fixed_params,vary_params in params_list:
     )
 df_MPS_0_T_DW=convert_pd(data_MPS_0_T_DW_dict,names=['Metrics','sm','sC','p_ctrl','L','p_proj',])
 
-def trajvar(df,L,p_ctrl,sC):
-    data=df.xs(L,level='L').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(sC,level='sC')
-
-    data_DW1=np.stack(df.xs(L,level='L').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(sC,level='sC').xs('DW1',level='Metrics')['observations'])
-    # fix SC, as a function T 
+def traj_qvar(df,L,p_ctrl,sC):
+    # trajecotry fluctuation, fix SC, as a function T 
+    data = df['observations'].xs(sC,level='sC').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(L,level='L')
+    data_DW1=np.stack(data.xs('DW1',level='Metrics'))
+    
     mask_traj_0=(data_DW1.std(axis=0)<err)
     mask_fixed_point_traj=(data_DW1.mean(axis=0)<err)
-    return mask_traj_0,mask_fixed_point_traj
-def qvar(df, L,p_ctrl,sC):
-    data_DW1=np.stack(df.xs(L,level='L').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(sC,level='sC').xs('DW1',level='Metrics')['observations'])
-    data_DW2=np.stack(df.xs(L,level='L').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(sC,level='sC').xs('DW2',level='Metrics')['observations'])
+
+    # state_fluctuation
+    data_DW2=np.stack(data.xs('DW2',level='Metrics'))
     qvar=(data_DW2-data_DW1**2)
     mask_qvar_0=(qvar<err)
     mask_fixed_point_qvar=(data_DW1<err)
-    return mask_qvar_0,mask_fixed_point_qvar
+
+    return mask_traj_0,mask_fixed_point_traj, mask_qvar_0,mask_fixed_point_qvar
+
 
 
 mask_traj_0_dict={}
@@ -73,19 +73,17 @@ for p in tqdm(params_list[0][1]['p_ctrl']):
         mask_fixed_point_qvar_dict[(p,L)]=[]
         for sC in range((params_list[0][1]['sC']).shape[0]):
             try:
-                mask_traj_0,mask_fixed_point_traj=trajvar(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC)
-                mask_qvar_0,mask_fixed_point_qvar=qvar(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC)
-
+                mask_traj_0,mask_fixed_point_traj,mask_qvar_0,mask_fixed_point_qvar=traj_qvar(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC)
                 mask_traj_0_dict[(p,L)].append(mask_traj_0)
                 mask_fixed_point_traj_dict[(p,L)].append(mask_fixed_point_traj)
                 mask_qvar_0_dict[(p,L)].append(mask_qvar_0)
                 mask_fixed_point_qvar_dict[(p,L)].append(mask_fixed_point_qvar)
             except:
                 pass
-        mask_traj_0_dict[(p,L)]=np.array(mask_traj_0_dict[(p,L)])
-        mask_fixed_point_traj_dict[(p,L)]=np.array(mask_fixed_point_traj_dict[(p,L)])
-        mask_qvar_0_dict[(p,L)]=np.array(mask_qvar_0_dict[(p,L)])
-        mask_fixed_point_qvar_dict[(p,L)]=np.array(mask_fixed_point_qvar_dict[(p,L)])
+        # mask_traj_0_dict[(p,L)]=np.array(mask_traj_0_dict[(p,L)])
+        # mask_fixed_point_traj_dict[(p,L)]=np.array(mask_fixed_point_traj_dict[(p,L)])
+        # mask_qvar_0_dict[(p,L)]=np.array(mask_qvar_0_dict[(p,L)])
+        # mask_fixed_point_qvar_dict[(p,L)]=np.array(mask_fixed_point_qvar_dict[(p,L)])
 
 with open(f'traj_state_L{L}.pickle','wb') as f:
     pickle.dump([mask_traj_0_dict,mask_fixed_point_traj_dict,mask_qvar_0_dict,mask_fixed_point_qvar_dict],f)
