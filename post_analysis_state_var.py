0f923c3c56f132eb0894db7d5dc6039f79343a1c
Haining Pan <hnpan@terpmail.umd.edu>
2025-01-07 12:33:18 -0500

    update post_analysis_state_var.py to save space

--- post_analysis_state_var.py
+++ post_analysis_state_var.py
@@ -1,3 +1,4 @@
+# This script is used to compute true state variance as in the paper, the clear distinction is that , it does not take the "trajector-average" of state fluctuation, which is consistent with the paper. Also to save the disk space, it automatically take out the "steady state" from L^2 to 2L^2
 import sys
 dir_path='../control_transition'
 sys.path.append(dir_path)
@@ -11,10 +12,10 @@ params_list=[
 ({'nu':0,'de':1,},
 {
 # 'p_ctrl':[.47,.49,.51,.53],
-# 'p_ctrl':[0.4,0.5,.6],
-'p_ctrl':[0.4,0.45,0.47,0.49,0.5,0.51,0.53,0.55,0.6],
+'p_ctrl':[0.4,0.5,.6],
+# 'p_ctrl':[0.4,0.45,0.47,0.49,0.5,0.51,0.53,0.55,0.6],
 'p_proj':np.linspace(0.0,0.0,1),
-'sC':np.arange(0,500),
+'sC':np.arange(0,5),
 'sm':np.arange(500),
 'L':[L,]
 }
@@ -23,6 +24,8 @@ params_list=[
 ob="DW"
 ob1=ob+'1'
 ob2=ob+'2'
+
+err=1e-5
 for fixed_params,vary_params in params_list:
     data_MPS_0_T_DW_dict=generate_params(
         fixed_params=fixed_params,
@@ -48,19 +51,29 @@ def state_var(df, L,p_ctrl,sC):
     data_ob1=np.stack(data.xs(ob1,level='Metrics'))
     data_ob2=np.stack(data.xs(ob2,level='Metrics'))
     return (data_ob2-data_ob1**2)
+def zero_ratio(state_var_):
+    data=state_var_[:,L**2:]
+    zero_fluct=(data<err).sum()
+    total_sample=np.prod(data.shape)
+    return zero_fluct,total_sample
 
-state_var_dict={}
+
+state_var_ratio_dict={}
 for p in tqdm(params_list[0][1]['p_ctrl']):
     for L in params_list[0][1]['L']:
         print(p,L)
-        state_var_dict[(p,L)]=[]
+        zero_fluct=0
+        total_sample=0
         for sC in range((params_list[0][1]['sC']).shape[0]):
             try:
-                state_var_dict[(p,L)].append(state_var(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC))
+                state_var_=state_var(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC)
+                zero_fluct_,total_sample_=zero_ratio(state_var_)
+                zero_fluct+=zero_fluct_
+                total_sample+=total_sample_
             except:
                 pass
-        state_var_dict[p,L]=np.vstack(state_var_dict[(p,L)])
+        state_var_ratio_dict[p,L]=zero_fluct/total_sample
         
 with open(f'state_var_C_m_T_L{L}.pickle','wb') as f:
 # with open(f'state_var_C_m_T_O_L{L}.pickle','wb') as f:
-    pickle.dump(state_var_dict,f)
+    pickle.dump(state_var_ratio_dict,f)
